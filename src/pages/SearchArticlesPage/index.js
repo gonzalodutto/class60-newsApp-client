e1cff535e9dcbb04069ec154c63e3582e86b99b3
gonzalodutto <gonzalodutto@gmail.com>
2022-08-13 22:35:37 +0200

    Some documentation in the code

--- src/pages/SearchArticlesPage/index.js
+++ src/pages/SearchArticlesPage/index.js
@@ -16,22 +16,38 @@ const SearchArticlePage = () => {
         const articlesResponse = await axios.get(
           "http://localhost:4000/articles"
         );
-        setArticleList(articlesResponse.data);
+
+        // Put in a constant the Articles filtered, is here so I can display hoy much items I finded with the filtering via length method
+        const articlesFiltered = articlesResponse.data.filter((articleObj) =>
+          articleObj.title.toLowerCase().includes(filter)
+        );
+
+        // Set the feched data in the useState
+        setArticleList(articlesFiltered);
       };
 
+      // Run function
       fetchData();
+
+      // If errase text in input, put again empty array
       if (params.filter) {
         setFilter(params.filter);
       } else {
         setFilter("");
       }
+
+      // If no data is feched, send an error message
     } catch (e) {
       console.log(e.message);
     }
-  }, [params.filter]);
+
+    // the useEffect is going to reload every time this values changes:
+  }, [params.filter, filter]);
 
   const updateFilter = (e) => {
+    // Update the useState of the filter
     setFilter(e.target.value);
+    // Navigate to the same adress, but with the search value in the end
     navigate(`/articles/${e.target.value}`);
   };
 
@@ -46,22 +62,19 @@ const SearchArticlePage = () => {
             onChange={updateFilter}
           />
         </div>
+        <div>{articleList ? articleList.length : "..."} articles found</div>
         <div>
           {articleList
-            ? articleList
-                .filter((characterObj) =>
-                  characterObj.title.toLowerCase().includes(filter)
-                )
-                .map((article, i) => (
-                  <ArticleCard
-                    key={i}
-                    id={article.id}
-                    author={article.author}
-                    img={article.imageUrl}
-                    title={article.title}
-                    categoryId={article.categoryId}
-                  />
-                ))
+            ? articleList.map((article, i) => (
+                <ArticleCard
+                  key={i}
+                  id={article.id}
+                  author={article.author}
+                  img={article.imageUrl}
+                  title={article.title}
+                  categoryId={article.categoryId}
+                />
+              ))
             : "loading..."}
         </div>
       </div>
